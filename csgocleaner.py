cd6135826aa536aa5e36626a80437127e2023fa4
stanislaw <49199507+stanico@users.noreply.github.com>
2022-03-12 22:43:42 +0200

    Update csgocleaner.py

--- csgocleaner.py
+++ csgocleaner.py
@@ -1,4 +1,4 @@
-# CSGO CLEANER v0.3 12/03/2022
+# CSGO CLEANER v0.4 12/03/2022
 # credits: stanico#1206
 # tg: @Stanico
 # github.com/stanico/csgocleaner/
@@ -8,10 +8,45 @@ import os
 import shutil
 import time
 from distutils.dir_util import copy_tree
+from progress.bar import IncrementalBar
+
+
+log = []
 
 
 def clear():
     os.system('cls')
+
+
+def language():
+    global lang
+    wr = ""
+    clear()
+    intro()
+    if not os.path.exists(main_path + '\\lang.txt'):
+        print('[1] Русский')
+        print('[2] English')
+        ans = input('Выберите язык / Select language: ')
+        if ans == '1':
+            wr = 'russian'
+            lang = lang_ru
+        if ans == '2':
+            wr = 'english'
+            lang = lang_en
+        with open(main_path + "\\lang.txt", "w") as file:
+            file.write(wr)
+            file.close()
+            clear()
+            intro()
+    else:
+        with open(main_path + "\\lang.txt", "r") as file:
+            a = file.read()
+            if a == "russian":
+                lang = lang_ru
+            if a == "english":
+                lang = lang_en
+            clear()
+            intro()
 
 
 def remove_file(path, file):
@@ -23,8 +58,8 @@ def remove_file(path, file):
         shutil.copy(os.path.join(csgo_path + path + file), os.path.join(main_path + path + file))
         os.remove(os.path.join(csgo_path + path + file))
     else:
-        print('[ERROR] FILE NOT EXISTS')
-        print(file + " " + path)
+        log.append('[ERROR] FILE NOT EXISTS ')
+        log.append(file + " " + path+" | ")
 
 
 def restore_file(path, file):
@@ -36,8 +71,8 @@ def restore_file(path, file):
         shutil.copy(os.path.join(main_path + path + file), os.path.join(csgo_path + path + file))
         os.remove(os.path.join(main_path + path + file))
     else:
-        print('remove_byextFILE NOT EXISTS')
-        print(file + " " + path)
+        log.append('[ERROR] FILE NOT EXISTS ')
+        log.append(file + " " + path+" |")
 
 
 def remove_tree(path):
@@ -49,7 +84,8 @@ def remove_tree(path):
         copy_tree(os.path.join(csgo_path + path), os.path.join(main_path + path))
         shutil.rmtree(os.path.join(csgo_path + path))
     else:
-        print('[ERROR] TREE NOT FOUND')
+        log.append('[ERROR] TREE NOT FOUND ')
+        log.append(path+" | ")
 
 
 def restore_tree(path):
@@ -61,7 +97,8 @@ def restore_tree(path):
         copy_tree(os.path.join(main_path + path), os.path.join(csgo_path + path))
         shutil.rmtree(os.path.join(main_path + path))
     else:
-        print('[ERROR] FILE NOT EXISTS')
+        log.append('[ERROR] TREE NOT EXISTS ')
+        log.append(path+' | ')
 
 
 def remove_byext(ext, path):
@@ -94,132 +131,241 @@ def clear_files(ext, path):
 def clean():
     global main_path
     if bots == "1" or stickers == "1" or videos == "1" or font == "1" or servers == "2":
-        print('Изменения после очистки: ')
+        print(lang[8])
     else:
         pass
     if bots == "1":
-        print(' - Не будут работать боты в локальной игре (На картах по типу AimBotz будут)')
+        print(lang[9])
     if stickers == "1":
-        print(' - Не будут видны стикеры на оружиях')
+        print(lang[10])
     if videos == "1":
-        print(' - Не будет заднего фона в меню (Будет просто черное)')
+        print(lang[11])
     if font == "1":
-        print(' - Будет системный шрифт, вместо обычного')
+        print(lang[12])
     if servers == "2":
-        print(' - Не будут работать сервера сообщества')
+        print(lang[13])
     print('')
-    print('Начить очистку?')
-    print('[1] Да')
-    print('[2] Нет')
-    b = input('Выберите действие: ')
+    print(lang[14])
+    print(lang[15])
+    print(lang[16])
+    b = input(lang[7])
     if b == '1':
         clear()
         intro()
-        print('Очистка может занять некоторое времья... зависит от ПК')
+        print(lang[17])
     else:
         main()
+    bar = IncrementalBar('Progress', max = 100)
     main_path = main_path + "\\restorepoint"
     # File Source: vk.com/boostfixteam
     remove_byext(".mdmp", "\\")
+    bar.next()
     remove_file("\\", "chrome.pak")
+    bar.next()
     remove_tree("\\directx_installer")
+    bar.next()
     remove_tree("\\EmplySteamDepot")
+    bar.next()
     # bin
     remove_tree("\\bin\\locales")
+    bar.next()
     remove_tree("\\bin\\map_publish")
+    bar.next()
     remove_tree("\\bin\\prefabs")
+    bar.next()
     remove_tree("\\bin\\v8_winxp")
+    bar.next()
     remove_file("\\bin\\", "convertdmx.lua")
+    bar.next()
     remove_file("\\bin\\", "libfbxsdk.dll")
+    bar.next()
     remove_file("\\bin\\", "maplist_csgo.txt")
+    bar.next()
     remove_file("\\bin\\", "mssdolby.flt")
+    bar.next()
     remove_file("\\bin\\", "mssdsp.flt")
+    bar.next()
     remove_file("\\bin\\", "msseax.flt")
+    bar.next()
     remove_file("\\bin\\", "msssrs.flt")
+    bar.next()
     remove_file("\\bin\\", "telemetry32c.dll")
+    bar.next()
     remove_file("\\bin\\", "trxtemplate.xml")
+    bar.next()
     remove_file("\\bin\\", "vidcfg.bin")
+    bar.next()
     # csgo
     remove_tree("\\csgo\\expressions")
+    bar.next()
     remove_tree("\\csgo\\materials")
+    bar.next()
     remove_tree("\\csgo\\models")
+    bar.next()
     remove_tree("\\csgo\\scenes")
+    bar.next()
     remove_tree("\\csgo\\resource")
+    bar.next()
     restore_tree("\\csgo\\resource\\overviews")
+    bar.next()
     restore_file("\\csgo\\resource\\ui\\", "buymenuconfig.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "valve_english.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "valve_russian.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "mp3player_english.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "boxrocket_english.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "dmecontrols_english.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "csgo_english.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "gameui_russian.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "chat_russian.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "sfui_russian.txt")
+    bar.next()
     restore_file("\\csgo\\resource\\", "csgo_russian.txt")
+    bar.next()
     if stickers == "1":
+        bar.next()
         pass
     else:
         restore_file("\\csgo\\resource\\", "vmtcache.txt")
+        bar.next()
     remove_tree("\\csgo\\scripts")
+    bar.next()
     remove_tree("\\csgo\\cache")
+    bar.next()
     restore_file("\\csgo\\scripts\\", "instructor_textures.txt")
+    bar.next()
     restore_file("\\csgo\\scripts\\", "inventory_structure.txt")
+    bar.next()
     restore_file("\\csgo\\scripts\\items\\", "items_game.txt")
+    bar.next()
     remove_file("\\csgo\\bin\\", "client_panorama.dll")
+    bar.next()
     remove_file("\\csgo\\", "modelsounds.cache")
+    bar.next()
     remove_file("\\csgo\\", "bspconvar_whitelist.txt")
+    bar.next()
     remove_file("\\csgo\\", "gamemodes_server.txt.example")
+    bar.next()
     remove_file("\\csgo\\", "demo_polish_settings.cfg")
+    bar.next()
     remove_file("\\csgo\\", "demoheader.tmp")
+    bar.next()
     remove_file("\\csgo\\", "gamerulescvars.txt.example")
+    bar.next()
     remove_file("\\csgo\\", "leaderboardsconfig.txt")
+    bar.next()
     remove_file("\\csgo\\", "lights.rad")
+    bar.next()
     remove_file("\\csgo\\", "loadouts.txt")
+    bar.next()
     remove_file("\\csgo\\", "maplist.txt")
+    bar.next()
     remove_file("\\csgo\\", "medalsconfig.txt")
+    bar.next()
     remove_file("\\csgo\\", "motd.txt")
+    bar.next()
     remove_file("\\csgo\\", "mapcycle.txt")
+    bar.next()
     remove_file("\\csgo\\", "missioncycle.txt")
+    bar.next()
     remove_file("\\csgo\\", "steam_appid.txt")
+    bar.next()
     remove_file("\\csgo\\", "navplace.db")
+    bar.next()
     remove_file("\\csgo\\", "pure_server_whitelist.txt")
+    bar.next()
     remove_file("\\csgo\\", "radial_quickinventory.txt")
+    bar.next()
     remove_file("\\csgo\\", "radial_radio.txt")
+    bar.next()
     remove_file("\\csgo\\", "scene.cache")
+    bar.next()
     remove_file("\\csgo\\", "serverconfig.vdf")
+    bar.next()
     remove_file("\\csgo\\", "splitscreen_config.txt")
+    bar.next()
     remove_file("\\csgo\\", "unusedcontent.cfg")
+    bar.next()
     remove_file("\\csgo\\", "whitelist.cfg")
+    bar.next()
     remove_file("\\csgo\\", "whitelist_beta.cfg")
+    bar.next()
     remove_tree("\\csgo\\maps\\soundcache")
+    bar.next()
     remove_tree("\\csgo\\maps\\graphs")
+    bar.next()
     remove_tree("\\csgo\\maps\\workshop")
+    bar.next()
     remove_byext(".txt", "\\csgo\\maps")
+    bar.next()
     remove_byext(".jpg", "\\csgo\\maps")
+    bar.next()
     clear_files(".cfg", "\\csgo\\maps\\cfg")
+    bar.next()
     if videos == "1":
         remove_tree("\\csgo\\panorama\\fonts")
+        bar.next()
+    else:
+        bar.next()
     if font == '1':
         remove_tree("\\csgo\\panorama\\videos")
+        bar.next()
+    else:
+        bar.next()
     # platform
     if servers == "2":
         remove_tree("\\platform\\materials")
+        bar.next()
         remove_tree("\\platform\\resource")
+        bar.next()
         remove_tree("\\platform\\scripts")
+        bar.next()
         remove_tree("\\platform\\servers")
+        bar.next()
         remove_tree("\\platform\\steam")
+        bar.next()
         remove_tree("\\platform\\vgui")
+        bar.next()
+    else:
+        bar.next()
+        bar.next()
+        bar.next()
+        bar.next()
+        bar.next()
+        bar.next()
     if bots == "1":
         remove_byext(".nav", "\\csgo\\maps")
+        bar.next()
         remove_file("\\csgo\\", "botchatter.db")
+        bar.next()
         remove_file("\\csgo\\", "botprofile.db")
+        bar.next()
         remove_file("\\csgo\\", "botprofilecoop.db")
+        bar.next()
+    else:
+        bar.next()
+        bar.next()
+        bar.next()
+        bar.next()
+    bar.next()
+    bar.next()
     clear()
     intro()
-    print('Успешно!')
-    print('Заходите в игру, и проверяйте изменения.')
-    print('Если стало хуже вы можете восстановить всё по умолчанию после повторного запуска программы!')
+    print(lang[18])
+    print(lang[19])
+    print(lang[20])
+    main_path = '%s\\csgocleaner' % os.environ['APPDATA']
+    with open(main_path +'\\log.txt', "w") as file:
+            file.write(' '.join(log))
+            file.close()
     time.sleep(5)
 
 
@@ -246,7 +392,7 @@ def intro():
     print('  / ____/ ___// ____/ __ \   / ____/ /   / ____/   |  / | / / ____/ __ \ ')
     print(' / /    \__ \/ / __/ / / /  / /   / /   / __/ / /| | /  |/ / __/ / /_/ / ')
     print('/ /___ ___/ / /_/ / /_/ /  / /___/ /___/ /___/ ___ |/ /|  / /___/ _, _/ ')
-    print('\____//____/\____/\____/   \____/_____/_____/_/  |_/_/ |_/v0.3_/_/ |_| ')
+    print('\____//____/\____/\____/   \____/_____/_____/_/  |_/_/ |_/v0.4_/_/ |_| ')
     print('Credits: stanico#1206')
     print('')
 
@@ -259,37 +405,37 @@ def setup():
     global servers
     clear()
     intro()
-    print('Отключить ботов? (На картах по типу AimBotz они будут!) [1/5]')
-    print('[1] Да')
-    print('[2] Нет')
-    bots = input('Выберите ответ: ')
+    print(lang[22])
+    print(lang[15])
+    print(lang[16])
+    bots = input(lang[21])
     clear()
     intro()
-    print('Отключить стикеры? (Их не будет видно на оружиях) [2/5]')
-    print('[1] Да')
-    print('[2] Нет')
-    stickers = input('Выберите ответ: ')
+    print(lang[23])
+    print(lang[15])
+    print(lang[16])
+    stickers = input(lang[21])
     clear()
     intro()
-    print('Отключить фон в меню? (В параметры запуска нужно будет вводить -novid) [3/5]')
-    print('[1] Да')
-    print('[2] Нет')
-    videos = input('Выберите ответ: ')
+    print(lang[24])
+    print(lang[15])
+    print(lang[16])
+    videos = input(lang[21])
     clear()
     intro()
-    print('Отключить шрифт? (Вместо обычного шрифта, будет системный) [4/5]')
-    print('[1] Да')
-    print('[2] Нет')
-    font = input('Выберите ответ: ')
+    print(lang[25])
+    print(lang[15])
+    print(lang[16])
+    font = input(lang[21])
     clear()
     intro()
-    print('Вы играете на серверах сообщества? [5/5]')
-    print('[1] Да')
-    print('[2] Нет')
-    servers = input('Выберите ответ: ')
+    print(lang[26])
+    print(lang[15])
+    print(lang[16])
+    servers = input(lang[21])
     clear()
     intro()
-    print('Настройка завершена успешно, теперь можно приступать к очистке!')
+    print(lang[27])
     time.sleep(0.5)
     clear()
     intro()
@@ -302,20 +448,22 @@ def main():
     clear()
     intro()
     if not os.path.exists(main_path):
-        print('Введите путь к папке CS:GO')
-        print('Пример: C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive')
+        os.makedirs(main_path)
+        language()
+        print(lang[0])
+        print(lang[1])
         csgo_path = input()
         if os.path.exists(csgo_path + "\\csgo.exe"):
-            os.makedirs(main_path)
+           
             with open(main_path + '\\path.txt', 'w') as file:
                 file.write(csgo_path)
                 file.close()
             clear()
             intro()
-            print("Примечание: Перед очисткой желательно посетить вкладку Настройка")
-            print('[1] Очистка')
-            print('[2] Настройка')
-            a = input("Выберите действие: ")
+            print(lang[2])
+            print(lang[3])
+            print(lang[4])
+            a = input(lang[7])
             if a == "1":
                 clear()
                 intro()
@@ -324,24 +472,29 @@ def main():
             if a == "2":
                 setup()
         else:
-            print('Не удалось найти csgo.exe!!!')
+            print(lang[28])
             input('')
             exit()
     else:
+        language()
         with open(main_path + '\\path.txt', 'r') as file:
             csgo_path = file.read()
             file.close()
-        print('[1] Очистка')
-        print('[2] Настройка')
+        print(lang[3])
+        print(lang[4])
         if os.path.exists(main_path + "\\restorepoint"):
-            print('[3] Возврат')
-            print('[4] Удалить точку восстановления')
-        a = input('Выберите действие: ')
+            print(lang[5])
+            print(lang[6])
+        a = input(lang[7] + ' ')
 
         if a == "1":
             clear()
             intro()
             clean()
+            main_path = '%s\\csgocleaner' % os.environ['APPDATA']
+            with open(main_path +'\\log.txt', "w") as file:
+                file.write(' AAA'.join(log))
+                file.close()
         if a == "2":
             setup()
 
@@ -349,15 +502,15 @@ def main():
             if a == "3":
                 clear()
                 intro()
-                print('Восстановление может занять некоторое время... зависит от ПК')
+                print(lang[29])
                 restore()
-                print('Успешно!')
+                print(lang[18])
                 time.sleep(2)
             if a == "4":
                 clear()
                 intro()
                 shutil.rmtree(main_path + "\\restorepoint")
-                print('Успешно!')
+                print(lang[18])
                 time.sleep(2)
 
 
@@ -368,6 +521,8 @@ servers = "2"
 stickers = "1"
 
 main_path = '%s\\csgocleaner' % os.environ['APPDATA']
+lang_ru = ['Введите путь к папке CS:GO', 'Пример: C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive', "Примечание: Перед очисткой желательно посетить вкладку Настройка", '[1] Очистка', '[2] Настройка', '[3] Возврат', '[4] Удалить точку восстановления', 'Выберите действие: ', 'Изменения после очистки: ', ' - Не будут работать боты в локальной игре (На картах по типу AimBotz будут)', ' - Не будут видны стикеры на оружиях', ' - Не будет заднего фона в меню (Будет просто черное)', ' - Будет системный шрифт, вместо обычного', ' - Не будут работать сервера сообщества', 'Начить очистку?', '[1] Да', '[2] Нет', 'Очистка может занять некоторое времья... зависит от ПК', 'Успешно!', 'Заходите в игру, и проверяйте изменения.', 'Если стало хуже вы можете восстановить всё по умолчанию после повторного запуска программы!', 'Выберите ответ: ', 'Отключить ботов? (На картах по типу AimBotz они будут!) [1/5]', 'Отключить стикеры? (Их не будет видно на оружиях) [2/5]', 'Отключить фон в меню? (В параметры запуска нужно будет вводить -novid) [3/5]', 'Отключить шрифт? (Вместо обычного шрифта, будет системный) [4/5]', 'Вы играете на серверах сообщества? [5/5]', 'Настройка завершена успешно, теперь можно приступать к очистке!', 'Не удалось найти csgo.exe!', 'Восстановление может занять некоторое время... зависит от ПК']
+lang_en = ['Enter the path to the CS:GO folder', 'Example: C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive', "Note: Before cleaning better to open the settings tab", '[1] Cleaning', '[2] Settings', '[3] Restore files', '[4] Remove restore point', 'Select action: ', 'Changes after cleanup: ', ' - Bots won`t work on local game (On maps like AimBotz they should work)', ' - Stickers on weapons will not be visible', ' - There will be no background in the menu (It will be just black)', ' - There will be a system font instead of default csgo font', ' - Community servers will not work', 'Start cleaning?', '[1] Yes', '[2] No', 'Cleaning up may take some time... depends on your PC', 'Successful!', 'Go into the game and check the changes.', 'If game gets worse you can restore everything into default after restarting the program!', 'Select answer: ', 'Disable bots? (Maps like AimBotz will have them!) [1/5]', "Disable stickers? (They won't be visible on weapons)", 'Disable menu background? (You will need to enter -novid in the launch options)', 'Disable font? (Instead of regular font, it will be system font)', 'Do you play on community servers? [5/5]', 'Setup completed successfully, now you can start cleaning!', 'Could not find csgo.exe!', 'Restoration may take some time... depends on the PC']
 
 
 main()
